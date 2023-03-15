af36c01b9799c8fb932273fd9db5a317bcd376a8
Harsh <imharshalashi333@gmail.com>
2023-03-16 00:21:11 +0530

    sidebar

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as ChannelDetail } from './ChannelDetail';
 export { default as SearchFeed } from './SearchFeed';
 export { default as VideoDetail } from './VideoDetail';
 export { default as Searchbar } from './Searchbar.jsx';
+export { default as Sidebar } from './Sidebar';
